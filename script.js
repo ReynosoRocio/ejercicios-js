f0284a8991780079936134f33b5798415a080ec2
ReynosoRocio <89603734+ReynosoRocio@users.noreply.github.com>
2022-01-24 23:38:41 -0600

    Solución a los ejercicios planteados
    Ejercicios resueltos

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const ejercicio1 = [
   3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
-  19, 88, 456, 13, 23, 24,
+  19, 88, 456, 13, 23, 24,1,
 ];
+
 
 const ejercicio2 = [
   {
@@ -35,3 +36,59 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+
+//Solucion al ejercicio 1
+let primos="Respuesta: ";
+
+ejercicio1.forEach(element => {
+  if(isPrime(element)){
+  primos+=element+', ';
+  }
+});
+
+document.getElementById('uno').innerHTML=primos.slice(0,-2)+'.';
+
+function isPrime(number) {
+  var times=0;
+  for(var i=1;i<=number;i++){
+    if(Number.isInteger(number/i)){
+      times++;
+      if(times>2){
+        return false;
+      }
+    }
+  }
+
+  if(times==2){
+    return true;
+  }else{
+    return false;
+  }
+
+
+}
+
+//Solución al ejercicio 2
+var usuarios="Pasan: ";
+ejercicio2.forEach(element=>{
+if(element.edad>=18&&element.esFamiliar){
+  usuarios+=element.nombre+", ";
+}
+});
+
+document.getElementById('dos').innerText=usuarios.slice(0,-2)+'.';
+
+//Solucion al ejercicio3
+var semilla=1;
+var sucesor=1;
+var fibo="1(1), 1(2), ";
+for(var i=1;i<49;i++){
+  aux=sucesor;
+  sucesor+=semilla;
+  semilla=aux;
+  fibo+=sucesor+"("+(i+2)+"), ";
+
+}
+
+document.getElementById('tres').innerText=fibo.slice(0,-2)+'.';
+
